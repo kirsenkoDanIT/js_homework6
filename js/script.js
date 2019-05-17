165072b39bfd012c2de7300af3551b43ed21c7f0
kirsenko <46247205+kirsenko@users.noreply.github.com>
2019-05-17 11:17:22 +0300

    add answer readme.md

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const arr = ['hello', 'world', false, 23, '23', null, undefined, true, [1, 2, '3
     name: 'value',
 }];
 const filterBy = (array, typeOfElem) => {
-    return array.filter(elem => (typeof elem) != typeOfElem);
+    return array.filter(elem => (typeof elem) !== typeOfElem);
 }
 
 console.log(filterBy(arr, 'string'))
